f1c66523fe0b80710df282198a8009e4bac8b912
bharatdubey <bharat1999dubey@gmail.com>
2025-07-09 22:44:21 +0530

    using_math_module

---
+++ PYTHON_PROGRAMMING_ESSENTIALS/Module_4/1.learning_modules.py
@@ -0,0 +1,32 @@
+"""
+Demonstration of importing modules.
+"""
+
+# Import the math module (look in the Docs for help)
+import math
+
+# Constants
+print("Math constants")
+print("==============")
+print(math.pi)
+print(f"{math.e:.2f}")
+print("")
+
+# Functions
+print("Math functions")
+print("==============")
+print(math.sqrt(25))
+print(math.trunc(14.83483))
+print(math.sin(math.pi / 2.0))
+print("")
+
+# Dir
+print("Dir")
+print("===")
+print(dir(math))
+print(dir(example))
+print("")
+
+print(example.message)
+print(math.__name__)
+print(example.__name__)
